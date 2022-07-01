9fb18ad6d3c19f6c88333e660a3c93386d5e62c7
Ching Yan Lee <106129893+LilyChing@users.noreply.github.com>
2022-07-01 20:48:40 +0800

    filter out symbol and word.length <=2

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // Define variable
 let wordList =[];
 let canPlay = true;
+// Get data from internet word list
 function getData(){
     return fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt")
         .then(response => response.text())
@@ -8,7 +9,19 @@ function getData(){
 // Please run *ALL* function inside main
 async function main(){
     const result = await getData(); //wait for fetch to finish
-    wordList = result.split("\n")/*.filter(char => /[a-zA-Z]+$/.test(char))*/;
+    wordList = result.split("\n").filter(word => filterWord(word));
     console.log(wordList);
 }
 main();
+// filter out symbol and word.length <=2
+function filterWord (word){
+    if (word.length <=2) {
+        return false;
+    }else if (/[.'*+?^${}()|[\]\\|0-9]|[/]/.test(word)){
+        return false;
+    }else if (word.includes('-')){
+        return false;
+    }else{
+        return true;
+    }
+}
